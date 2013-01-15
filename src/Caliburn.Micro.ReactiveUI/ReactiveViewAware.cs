5579057e10fc0e97d303e125217b7eb1eba44e4d
Brendan Forster <me@brendanforster.com>
2013-01-15 19:39:59 +1100

    added shims to map the old Reflection APIs off Type to the new TypeInfo ones

--- src/Caliburn.Micro.ReactiveUI/ReactiveViewAware.cs
+++ src/Caliburn.Micro.ReactiveUI/ReactiveViewAware.cs
@@ -3,7 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+#if NETFX_CORE
+using Windows.UI.Xaml;
+#else
 using System.Windows;
+#endif
+
 
 namespace Caliburn.Micro.ReactiveUI
 {
---
+++ src/Caliburn.Micro.ReactiveUI/TypeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Caliburn.Micro.ReactiveUI
+{
+    public static class TypeExtensions
+    {
+        public static IEnumerable<PropertyInfo> GetProperties(this Type type)
+        {
+            return type.GetTypeInfo().DeclaredProperties;
+        }
+
+        public static PropertyInfo GetProperty(this Type type, string name)
+        {
+            return type.GetTypeInfo().DeclaredProperties.FirstOrDefault(p => p.Name == name);
+        }
+
+        public static MethodInfo GetMethod(this Type type, string name)
+        {
+            return type.GetTypeInfo().DeclaredMethods.FirstOrDefault(p => p.Name == name);
+        }
+    }
+}
