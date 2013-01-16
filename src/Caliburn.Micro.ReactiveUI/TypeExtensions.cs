16967f15076c974261ea627e2b216842c082b8dc
Brendan Forster <me@brendanforster.com>
2013-01-17 10:30:33 +1100

    added the test project and stubbed some tests for TypeExtensions

--- src/Caliburn.Micro.ReactiveUI/TypeExtensions.cs
+++ src/Caliburn.Micro.ReactiveUI/TypeExtensions.cs
@@ -7,19 +7,29 @@ namespace Caliburn.Micro.ReactiveUI
 {
     public static class TypeExtensions
     {
+        /// <summary>
+        /// Returns all the public properties of the current Type.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public static IEnumerable<PropertyInfo> GetProperties(this Type type)
         {
-            return type.GetTypeInfo().DeclaredProperties;
+            return type.GetTypeInfo().DeclaredProperties
+                        .Where(p => p.GetMethod.IsPublic)
+                        .Where(p => p.SetMethod.IsPublic);
         }
 
         public static PropertyInfo GetProperty(this Type type, string name)
         {
-            return type.GetTypeInfo().DeclaredProperties.FirstOrDefault(p => p.Name == name);
+            return type.GetProperties().FirstOrDefault(p => p.Name == name);
         }
 
         public static MethodInfo GetMethod(this Type type, string name)
         {
-            return type.GetTypeInfo().DeclaredMethods.FirstOrDefault(p => p.Name == name);
+            return type.GetTypeInfo()
+                       .DeclaredMethods
+                       .Where(m => m.IsPublic)
+                       .FirstOrDefault(p => p.Name == name);
         }
     }
 }
