fbf7357b6df16f4e42666c1a569eb9a07655db9a
Brendan Forster <me@brendanforster.com>
2013-01-21 09:04:13 +1100

    updated build script so that it can be invoked from any arbitrary folder

--- nuget/build-package.ps1
+++ nuget/build-package.ps1
@@ -1,1 +1,10 @@
-..\src\.nuget\NuGet.exe pack .\package\caliburn.micro.reactiveui.nuspec
+function Get-ScriptDirectory
+{
+   $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+   Split-Path $Invocation.MyCommand.Path
+}
+
+$nuget = Join-Path (Get-ScriptDirectory) ..\src\.nuget\NuGet.exe
+$nuspec = Join-Path (Get-ScriptDirectory) .\package\caliburn.micro.reactiveui.nuspec
+
+. $nuget pack $nuspec
