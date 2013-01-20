5452a03b3aaf8a1488f03626a7e27df818cf31a7
Brendan Forster <me@brendanforster.com>
2013-01-21 09:10:30 +1100

    added msbuild call to package script

--- nuget/build-package.ps1
+++ nuget/build-package.ps1
@@ -4,7 +4,17 @@ function Get-ScriptDirectory
    Split-Path $Invocation.MyCommand.Path
 }
 
+# build the solution from scratch
+$version = "v4.0.30319"
+$sln = Join-Path (Get-ScriptDirectory) ..\src\Caliburn.Micro.ReactiveUI.sln
+
+. $env:windir\Microsoft.NET\Framework\$version\MSBuild.exe $sln /t:Rebuild /p:Configuration=Release /m /v:q
+
+# package it up
+
 $nuget = Join-Path (Get-ScriptDirectory) ..\src\.nuget\NuGet.exe
 $nuspec = Join-Path (Get-ScriptDirectory) .\package\caliburn.micro.reactiveui.nuspec
 
 . $nuget pack $nuspec
+
+
