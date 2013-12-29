85be6b25f70bdc5f7cdec3987270923438b34889
Damien Chaib <dchaib@gmail.com>
2013-12-29 19:01:56 +0100

    SimpleNavigation sample with Caliburn.Micro only

---
+++ samples/SimpleNavigation/SimpleNavigation/AppBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+
+namespace SimpleNavigation
+{    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Start();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ samples/SimpleNavigation/SimpleNavigation/ShellViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+
+namespace SimpleNavigation
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+            ShowPageOne();
+        }
+
+        public void ShowPageOne()
+        {
+            ActivateItem(new PageOneViewModel());
+        }
+
+        public void ShowPageTwo()
+        {
+            ActivateItem(new PageTwoViewModel());
+        }
+    }
+}
