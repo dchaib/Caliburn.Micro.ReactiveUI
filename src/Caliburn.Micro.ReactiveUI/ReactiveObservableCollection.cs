985134493511c1d9ea437d5c9993e170009074d5
Damien Chaib <dchaib@gmail.com>
2014-09-20 11:31:41 +0200

    Created ReactiveObservableCollection<T>

---
+++ src/Caliburn.Micro.ReactiveUI/ReactiveObservableCollection.cs
@@ -0,0 +1,40 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caliburn.Micro.ReactiveUI
+{
+    public class ReactiveObservableCollection<T> : ReactiveList<T>, IObservableCollection<T>
+    {
+        public bool IsNotifying { get; set; }
+
+        /// <summary>
+        /// Notifies subscribers of the property change.
+        /// </summary>
+        /// <param name = "propertyName">Name of the property.</param>
+        public void NotifyOfPropertyChange(string propertyName)
+        {
+            this.RaisePropertyChanged(propertyName);
+        }
+
+        /// <summary>
+        //  Raises a change notification indicating that all bindings should be refreshed.
+        /// </summary>
+        public void Refresh()
+        {
+            NotifyOfPropertyChange(string.Empty);
+        }
+
+        /// <summary>
+        /// Removes items.
+        /// </summary>
+        /// <param name="items">The items to be removed.</param>
+        public void RemoveRange(IEnumerable<T> items)
+        {
+            RemoveAll(items);
+        }
+    }
+}
